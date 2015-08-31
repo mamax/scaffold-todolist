3ca621a23609f9c15f687dc7e569e80e7f72bca5
Maksym Mazurkevych <mmmazurkevych@gmail.com>
2015-08-31 14:26:42 +0300

    project-list functionality

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,5 +1,6 @@
 class Task < ActiveRecord::Base
   belongs_to :project
+  validates :project_id, presence: true
   validates :content, presence: true, length: { maximum: 70}
-  default_scope { order('position ASC')}
+  default_scope { order('position ASC') }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :projects
+  has_many :projects, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,:omniauthable, :omniauth_providers => [:facebook], stretches: 20
 
